0b693d896125af150e48fac910f0b2a2e8b8c425
Rabby Hasan <rabby@bs-23.net>
2020-10-05 22:35:18 +0600

    added worker service

---
+++ Berger.Worker/Worker.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Berger.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private HttpClient _client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _client = new HttpClient();
+            _logger.LogInformation("Worker Started {date}", DateTime.Now);
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _client.Dispose();
+            _logger.LogInformation("Worker Stopped {date}", DateTime.Now);
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //await _client.PostAsync();
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
