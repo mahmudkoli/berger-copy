a20f448af5f32a4eddb146009664fcfd24141fd5
roy-subrata <roy.subrata.cs@gmail.com>
2020-11-21 15:41:57 +0600

    Revert To Resolve Commit

--- BergerMsfaApi/Mappings/IMapFrom.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutoMapper;
-
-namespace BergerMsfaApi.Mappings
-{
-    public interface IMapFrom<T>
-    {
-        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType()).ReverseMap();
-    }
-}
