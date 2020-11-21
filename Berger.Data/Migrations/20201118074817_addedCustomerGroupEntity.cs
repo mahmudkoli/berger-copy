0b26705a95f436bc162d973d557bbb9f77ad2086
roy-subrata <roy.subrata.cs@gmail.com>
2020-11-21 18:14:50 +0600

    Resolve-3

---
+++ Berger.Data/Migrations/20201118074817_addedCustomerGroupEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Berger.Data.Migrations
+{
+    public partial class addedCustomerGroupEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerGroups",
+                columns: table => new
+                {
+                    CustomerAccountGroup = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerGroups");
+        }
+    }
+}
