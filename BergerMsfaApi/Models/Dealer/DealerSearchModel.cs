ac656fc9eed267065dbe62439e2fe3fc5c557442
Mahmud Koli <mahmudkolirpi@gmail.com>
2021-02-14 14:57:28 +0600

    get dealer by category updated

---
+++ BergerMsfaApi/Models/Dealer/DealerSearchModel.cs
@@ -0,0 +1,18 @@
+﻿using Berger.Common.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BergerMsfaApi.Models.Dealer
+{
+    public class AppDealerSearchModel
+    {
+        public string UserCategory { get; set; }
+        public List<string> UserCategoryIds { get; set; }
+        public string DealerName { get; set; }
+        public EnumDealerCategory? DealerCategory { get; set; }
+        public int? PageNo { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
