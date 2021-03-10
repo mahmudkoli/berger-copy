20f4b49ed2afc2f38dcc09b3b31daba3352f04de
Ashrafuzzaman Remon <remon024@gmail.com>
2021-03-10 13:21:34 +0600

    Add Bandinfo and dealerinfo log table

---
+++ Berger.Data/MsfaEntity/SAPTables/BrandInfoStatusLog.cs
@@ -0,0 +1,21 @@
+﻿using Berger.Data.Common;
+using Berger.Data.MsfaEntity.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Berger.Data.MsfaEntity.SAPTables
+{
+   public class DealerInfoStatusLog: AuditableEntity<int>
+    {
+        public int UserId { get; set; }
+        public int DealerInfoId { get; set; }
+        public DealerInfo DealerInfo { get; set; }
+
+        public UserInfo User { get; set; }
+
+        public string PropertyValue { get; set; }
+        public string PropertyName { get; set; }
+
+    }
+}
