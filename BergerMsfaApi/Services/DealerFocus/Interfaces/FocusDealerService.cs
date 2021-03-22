72ccec1557beaa777bf922cf53cd6508b3d3f87c
Abu Bakar Siddique <ab.siddique@brainstation23.com>
2021-03-22 15:27:35 +0600

    Dealer Info, Dealer Status Log Updated

--- BergerMsfaApi/Services/DealerFocus/Interfaces/FocusDealerService.cs
+++ BergerMsfaApi/Services/DealerFocus/Interfaces/FocusDealerService.cs
@@ -19,15 +19,18 @@ namespace BergerMsfaApi.Services.DealerFocus.Interfaces
         private readonly IRepository<FocusDealer> _focusDealer;
         private readonly IRepository<UserInfo> _userInfoSvc;
         private readonly IRepository<DealerInfo> _dealerInfo;
+        private readonly IRepository<DealerInfoStatusLog> _dealerInfoStatusLog;
         public FocusDealerService(
             IRepository<FocusDealer> focusDealer,
             IRepository<UserInfo> userInfoSvc,
-            IRepository<DealerInfo> dealerInfo
+            IRepository<DealerInfo> dealerInfo,
+            IRepository<DealerInfoStatusLog> dealerInfoStatusLog
             )
         {
             _focusDealer = focusDealer;
             _userInfoSvc = userInfoSvc;
             _dealerInfo = dealerInfo;
+            _dealerInfoStatusLog = dealerInfoStatusLog;
         }
 
         public async Task<IPagedList<FocusDealerModel>> GetFocusdealerListPaging(int index,int pageSize,string search)
@@ -96,7 +99,9 @@ namespace BergerMsfaApi.Services.DealerFocus.Interfaces
                 AccountGroup = s.AccountGroup,ContactNo = s.ContactNo,Area = s.SalesGroup, CustZone = s.CustZone,
                 BusinessArea = s.BusinessArea,IsExclusiveLabel = s.IsExclusive ? "Exclusive" : "Non Exclusive",
                 IsCBInstalledLabel = s.IsCBInstalled ? "Installed" : "Not Installed", IsCBInstalled = s.IsCBInstalled,
-                IsExclusive = s.IsExclusive
+                IsExclusive = s.IsExclusive, IsLastYearAppointedLabel = s.IsLastYearAppointed ? "Last Year Appointed" : "Not Appointed",
+                IsClubSupremeLabel = s.IsClubSupreme ? "Club Supreme" : "Not Club Supreme", IsLastYearAppointed = s.IsLastYearAppointed,
+                IsClubSupreme = s.IsClubSupreme
             }).ToList();
 
             if (!string.IsNullOrEmpty(search)) dealers = dealers.Search(search);
@@ -110,9 +115,37 @@ namespace BergerMsfaApi.Services.DealerFocus.Interfaces
         {
             var find = await _dealerInfo.FindAsync(f => f.CustomerNo == dealer.CustomerNo);
             if (find == null) return false;
+
+            await CreateDealerInfoStatusLog(dealer);
+
             find.IsCBInstalled = dealer.IsCBInstalled;
             find.IsExclusive = dealer.IsExclusive;
+            find.IsLastYearAppointed = dealer.IsLastYearAppointed;
+            find.IsClubSupreme = dealer.IsClubSupreme;
             await _dealerInfo.UpdateAsync(find);
+            return true;
+        }
+        public async Task<bool> CreateDealerInfoStatusLog(DealerInfo dealer)
+        {
+            var find = await _dealerInfo.FindAsync(f => f.CustomerNo == dealer.CustomerNo);
+            if (find == null) return false;
+            try
+            {
+                var dealerInfoStatusLog = new DealerInfoStatusLog()
+                {
+                    DealerInfoId = find.Id,
+                    UserId = AppIdentity.AppUser.UserId,
+                    PropertyName = find.IsExclusive != dealer.IsExclusive ? "IsExclusive" : "IsCBInstalled",
+                    PropertyValue = find.IsExclusive != dealer.IsExclusive ?
+                    (dealer.IsExclusive ? "Exclusive" : "Non Exclusive") :
+                    (dealer.IsCBInstalled ? "Installed" : "Not Installed")
+                };
+                await _dealerInfoStatusLog.CreateAsync(dealerInfoStatusLog);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             return true;
         }
         #endregion
