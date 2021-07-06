ed9a3cdfcda0c4fb6fa3c565956e1719806690ae
Ashrafuzzaman Remon <remon024@gmail.com>
2021-07-06 14:35:36 +0600

    set null emprole in menu permission

---
+++ Berger.Data/Migrations/20210706083422_set-null-emproleId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Berger.Data.Migrations
+{
+    public partial class setnullemproleId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EmpRoleId",
+                table: "MenuPermissions",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "EmpRoleId",
+                table: "MenuPermissions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
