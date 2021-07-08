dc94da1ecc23f38f245d97f76c78f1858fed30f5
Mustafizur Rahman <mustafizur@bs-23.net>
2021-07-08 16:51:03 +0600

    SyncDailyTargetLog year and month

---
+++ Berger.Data/Migrations/20210708105010_SyncDailyTargetLog-Year-month.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Berger.Data.Migrations
+{
+    public partial class SyncDailyTargetLogYearmonth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "SyncDailyTargetLogs");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Month",
+                table: "SyncDailyTargetLogs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "SyncDailyTargetLogs",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Month",
+                table: "SyncDailyTargetLogs");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "SyncDailyTargetLogs");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "SyncDailyTargetLogs",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
