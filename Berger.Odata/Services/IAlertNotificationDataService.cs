ed389a5d2646452bf08fdc0de8265e90ac5edca0
Ashrafuzzaman Remon <remon024@gmail.com>
2021-07-08 13:04:33 +0600

    add alert notification service

---
+++ Berger.Odata/Services/IAlertNotificationDataService.cs
@@ -0,0 +1,17 @@
+﻿using Berger.Odata.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berger.Odata.Services
+{
+    public interface IAlertNotificationDataService
+    {
+        Task<IList<AppChequeBounceNotificationModel>> GetAllTodayCheckBouncesByDealerIds(List<string> dealerIds);
+        Task<IList<AppCreditLimitCrossNotificationModel>> GetAllTodayCreditLimitCrossByDealerIds(List<string> dealerIds);
+        Task<IList<AppPaymentFollowUpNotificationModel>> GetAllTodayPaymentFollowUpByDealerIds(List<string> dealerIds);
+        Task<IList<AppCustomerOccasionNotificationModel>> GetAllTodayCustomerOccasionsByDealerIds(List<string> dealerIds);
+
+    }
+}
