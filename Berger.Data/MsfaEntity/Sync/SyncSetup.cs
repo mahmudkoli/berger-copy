ce91dae28a6d4e6c37a1de12f7d5acb587629d04
Mustafizur Rahman <mustafizur@bs-23.net>
2021-07-10 21:30:36 +0600

    SyncSetups addded

---
+++ Berger.Data/MsfaEntity/Sync/SyncSetup.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Berger.Data.Common;
+
+namespace Berger.Data.MsfaEntity.Sync
+{
+    public class SyncSetup : AuditableEntity<int>
+    {
+        public int DailySyncCount { get; set; }
+        public DateTime? LastSyncTime { get; set; }
+    }
+}
