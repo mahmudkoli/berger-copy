b68e9ad59f8578e6c6edd67921a168a37e661197
Ashrafuzzaman Remon <remon024@gmail.com>
2021-07-15 11:16:06 +0600

    edit-posting-and-clear-datetime-formate

---
+++ Berger.Data/Migrations/20210715051211_edit-posting-and-clear-datetime-formate.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Berger.Data.Migrations
+{
+    public partial class editpostingandcleardatetimeformate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "InvoiceDate",
+                table: "PaymentFollowup",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostingDate",
+                table: "PaymentFollowup",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ClearDate",
+                table: "ChequeBounceNotification",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PostingDate",
+                table: "ChequeBounceNotification",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PostingDate",
+                table: "PaymentFollowup");
+
+            migrationBuilder.DropColumn(
+                name: "PostingDate",
+                table: "ChequeBounceNotification");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "InvoiceDate",
+                table: "PaymentFollowup",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ClearDate",
+                table: "ChequeBounceNotification",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
