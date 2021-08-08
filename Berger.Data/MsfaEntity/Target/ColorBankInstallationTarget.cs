70967496b478133056fc75d700beed3a86dd02c4
Mustafizur Rahman <mustafizur@bs-23.net>
2021-08-08 19:23:38 +0600

    ColorBankInstallationTarget added

---
+++ Berger.Data/MsfaEntity/Target/ColorBankInstallationTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Berger.Data.Common;
+
+namespace Berger.Data.MsfaEntity.Target
+{
+    public class ColorBankInstallationTarget : AuditableEntity<int>
+    {
+        [StringLength(50)]
+        public string BusinessArea { get; set; }
+        [StringLength(50)]
+        public string Territory { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Target { get; set; }
+
+    }
+}
