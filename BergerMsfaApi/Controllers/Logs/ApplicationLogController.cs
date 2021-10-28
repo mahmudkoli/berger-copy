b9d19165421c6d36e3e4d57064939157b70a0a38
Mahmud Koli <mahmudkolirpi@gmail.com>
2021-10-28 21:10:49 +0600

    App Log added

---
+++ BergerMsfaApi/Controllers/Logs/ApplicationLogController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using BergerMsfaApi.Controllers.Common;
+using BergerMsfaApi.Models.Logs;
+using BergerMsfaApi.Services.Logs.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BergerMsfaApi.Controllers.Logs
+{
+    [ApiVersion("1")]
+    [Route("api/v{v:apiVersion}/[controller]")]
+    public class ApplicationLogController : BaseController
+    {
+        private readonly IApplicationLogService _mobileAppLogService;
+
+        public ApplicationLogController(
+            IApplicationLogService mobileAppLogService)
+        {
+            _mobileAppLogService = mobileAppLogService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("CreateAppLog")]
+        public async Task<IActionResult> CreateDealerSalesCall([FromBody] MobileAppLogModel model)
+        {
+            try
+            {
+                var result = await _mobileAppLogService.AddMobileAppLogAsync(model);
+                return OkResult(result);
+            }
+            catch (Exception ex)
+            {
+                return ExceptionResult(ex);
+            }
+        }
+    }
+}
---
+++ BergerMsfaApi/Services/Logs/Implementation/ApplicationLogService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Berger.Common.Enumerations;
+using Berger.Data.MsfaEntity.Logs;
+using Berger.Data.MsfaEntity.Master;
+using Berger.Data.MsfaEntity.Setup;
+using Berger.Data.MsfaEntity.Tinting;
+using BergerMsfaApi.Extensions;
+using BergerMsfaApi.Models.Common;
+using BergerMsfaApi.Models.Logs;
+using BergerMsfaApi.Models.Tinting;
+using BergerMsfaApi.Repositories;
+using BergerMsfaApi.Services.Logs.Interfaces;
+using BergerMsfaApi.Services.Setup.Interfaces;
+using BergerMsfaApi.Services.Tinting.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace BergerMsfaApi.Services.Logs.Implementation
+{
+    public class ApplicationLogService : IApplicationLogService
+    {
+        public readonly IRepository<MobileAppLog> _mobileAppLogSvc;
+        public readonly IMapper _mapper;
+
+        public ApplicationLogService(
+              IRepository<MobileAppLog> mobileAppLogSvc,
+              IMapper mapper
+            )
+        {
+            _mobileAppLogSvc = mobileAppLogSvc;
+            _mapper = mapper;
+        }
+
+        public async Task<bool> AddMobileAppLogAsync(MobileAppLogModel model)
+        {
+            try
+            {
+                var entity = _mapper.Map<MobileAppLog>(model);
+                await _mobileAppLogSvc.CreateAsync(entity);
+                await _mobileAppLogSvc.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+            }
+
+            return true;
+        }
+    }
+}
